df9137076f4273d3a679fcdfc1ba88b66539b093
Yifan Wu <yifanwu@10-18-153-248.dynapool.wireless.nyu.edu>
2023-03-01 14:09:18 -0500

    add django tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+from polls.models import *
+from django.utils import timezone
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        q = Question.objects.create(question_text="To be or not to be, THAT is the question.", pub_date=timezone.now())
+        Choice.objects.create(question=q, choice_text="To be")
+
+    def test_question(self):
+        q = Question.objects.get(question_text="To be or not to be, THAT is the question.")
+        self.assertEqual(q.was_published_recently(), True)
