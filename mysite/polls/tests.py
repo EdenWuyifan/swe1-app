caff6550793cab56232a7e04cfe18979a0a4d9d7
Yifan Wu <yifanwu@YifandeMacBook-Pro.local>
2023-03-02 22:03:42 -0500

    update for black

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -5,11 +5,17 @@ from django.test import TestCase
 from .models import *
 from django.utils import timezone
 
+
 class ModelTestCase(TestCase):
     def setUp(self):
-        q = Question.objects.create(question_text="To be or not to be, THAT is the question.", pub_date=timezone.now())
+        q = Question.objects.create(
+            question_text="To be or not to be, THAT is the question.",
+            pub_date=timezone.now(),
+        )
         Choice.objects.create(question=q, choice_text="To be")
 
     def test_question(self):
-        q = Question.objects.get(question_text="To be or not to be, THAT is the question.")
+        q = Question.objects.get(
+            question_text="To be or not to be, THAT is the question."
+        )
         self.assertEqual(q.was_published_recently(), True)
